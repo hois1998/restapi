49350b3fa7556460eee0e3ac2888dd74d1f2ecba
younghoSNU <hois1998@snu.ac.kr>
2021-06-06 13:49:20 +0900

    add routes/test.js

--- routes/test.js
+++ routes/test.js
@@ -25,6 +25,62 @@
 //   ffmpegId.kill('SIGINT')}, 60000);
 
 
-setTimeout(() => {
-  process.exit(1);
-}, 5000);
+/*
+return array which has entry type of object
+
+*/
+const mysql = require('mysql2/promise');
+
+const mysqlConnnectionOpt = require('/home/ubuntu/rest_api/Rest_API_Server/restapi/config/mysql_connection_option');
+
+let  connection; 
+
+async function getJSON(tablename, supervNum) {
+  try {
+    connection = await mysql.createConnection(mysqlConnnectionOpt);
+    const column0 = 'id';
+    const column1 = 'streamkey';
+    const column2 = 'mac';
+
+    const [rows, fields] = await connection.execute("SELECT "+column0+" , "+column1+" , "+column2+" FROM "+ tablename +" WHERE supervNum= '"+supervNum+"'");
+
+    console.log('streamkey_list_final', rows, rows.length);
+    connection.end();
+
+    return rows;
+  } catch (err) {
+    connection.end();
+    console.log(err);
+    throw new Error("123");
+  }
+};
+
+let rtmp_live_url = 'rtmp://123/';
+(async function () {
+try {
+  
+    let tablename = 'temperary_midterm_20210517_1500_1501';
+    supervNum = process.argv[2]
+    let streamkey_id_mac_list = await getJSON(tablename, supervNum).catch(err => {throw err;});
+
+    let endpoint_list = {};
+
+    for (let streamkey_id_mac of streamkey_id_mac_list) {
+      let {id, streamkey, mac} = streamkey_id_mac;
+      let arraySize = 3 //three streamkey for one student e.g. pc display, pc cam, phone
+      if (!endpoint_list.hasOwnProperty(id)) {
+        endpoint_list[id] = Array(3).fill(null);
+      }
+
+      if (streamkey != 'null') {
+        endpoint_list[id][parseInt(mac)] = rtmp_live_url + streamkey;
+
+      }
+    }
+
+    console.log('result of endpoint_list');
+    console.log( JSON.stringify(endpoint_list));
+} catch (err) {
+  console.log('final catch blk passed', err);
+}
+})();
